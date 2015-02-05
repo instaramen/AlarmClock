7a15908fe514c5606a4b04dcfcb2925de42a3652
Peter Tran <tranpeter93@gmail.com>
2015-02-04 17:39:27 -0800

    Lab05 First Commit

--- js/clock.js
+++ js/clock.js
@@ -18,18 +18,6 @@ var updateBackground = function(data) {
 	$('body').addClass(icon);
 }
 
-var getTemp = function() {
-	var key = '361ead2f6a85c726334fc78f16c74e19';
-	var link = 'https://api.forecast.io/forecast/'+key+'/35.300399,-120.662362?callback=?';
-	$(document).ready(function() {
-		$.getJSON (link, function(data) {
-			$('#forecastLabel').html(data['daily'].summary);			
-			$('#forecastIcon').attr('src', 'img/'+data['daily'].icon+'.png');
-			updateBackground(data);
-		});
-	});
-}
-
 var showAlarmPopup = function() {
 	$('#mask').removeClass('hide');
 	$('#popup').removeClass('hide');
@@ -40,10 +28,28 @@ var hideAlarmPopup = function() {
 	$('#popup').addClass('hide');
 }
 
-var insertAlarm = function(hours, mins, ampm, alarmName) {
+var deleteAlarm = function(event) {
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    query.get(event.data.id, {
+        success: function(result) {
+           result.destroy({});
+           $('#'+event.data.id).remove();
+        },
+    });
+}
+
+var insertAlarm = function(hours, mins, ampm, alarmName, id) {
 	var newDiv = $('<div>').addClass('flexable');
-	newDiv = newDiv.append('<div>').addClass('name').html(alarmName);
-	newDiv = newDiv.append('<div>').addClass('time').html(hours+':'+mins+':'+ampm);
+	var nameDiv = $('<div>'+alarmName+'</div>').addClass('name');
+	var timeDiv = $('<div>'+hours+':'+mins+':'+ampm+'</div>').addClass('time');
+	var del = $("<input type='button' value='x' class='deletion''>").click({id: id}, deleteAlarm);
+	newDiv.attr('id', id);
+
+	newDiv.append(nameDiv);
+	newDiv.append(timeDiv);
+	newDiv.append(del);
+
 	$('#alarms').append(newDiv);
 }
 
@@ -52,9 +58,43 @@ var addAlarm = function() {
 	h = $('#hours option:selected').text(); 
 	m = $('#mins option:selected').text(); 
 	ampm = $('#ampm option:selected').text(); 
-	name = $('#alarmName').text();
+	name = $('#alarmName').val();
 
-	insertAlarm(h, m, ampm, name);
+	var AlarmObject = Parse.Object.extend("Alarm");
+    var alarmObject = new AlarmObject();
+    alarmObject.save({"hours": h, 'mins': m, 'ampm': ampm, "alarmName": name}, {
+        success: function(object) {
+            insertAlarm(h, m, ampm, name, alarmObject.id);
+            hideAlarmPopup();
+        }
+    });	
+}
+
+var getAllAlarms = function() {
+	Parse.initialize('XKz9LmIyfMKHCM5QSiQdWGcx2neaOjF8AhNI2me9', 'bXQ318Qh1rxSmjO3ukb25kYCeiolsi8lC8tQnMTq');	
+
+    var AlarmObject = Parse.Object.extend("Alarm");
+    var query = new Parse.Query(AlarmObject);
+    query.find({
+        success: function(results) {
+          for (var i = 0; i < results.length; i++) { 
+             insertAlarm(results[i].get('hours'), results[i].get('mins'), results[i].get('ampm'), results[i].get('alarmName'), results[i].id);
+          }
+        }
+    });
+}
+
+var getTemp = function() {
+	var key = '361ead2f6a85c726334fc78f16c74e19';
+	var link = 'https://api.forecast.io/forecast/'+key+'/35.300399,-120.662362?callback=?';
+	$(document).ready(function() {
+		$.getJSON (link, function(data) {
+			$('#forecastLabel').html(data['daily'].summary);			
+			$('#forecastIcon').attr('src', 'img/'+data['daily'].icon+'.png');
+			updateBackground(data);
+		});
+		getAllAlarms();
+	});
 }
 
 getTemp();
