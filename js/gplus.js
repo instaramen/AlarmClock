4b0e1d534f58c3be5bfe7e4cdee243875932ed58
Peter Tran <tranpeter93@gmail.com>
2015-02-18 13:34:39 -0800

    HTML Gplus button

--- js/gplus.js
+++ js/gplus.js
@@ -1,10 +1,8 @@
-function onSignInCallback(authResult) {
-  console.log('doing a callback yo');
+function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
     // Update the app to reflect a signed in user
     // Hide the sign-in button now that the user is authorized, for example:
-    // document.getElementById('signinButton').setAttribute('style', 'display: none');
-    console.log('Success: Signed in');
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
   } else {
     // Update the app to reflect a signed out user
     // Possible error values:
@@ -12,5 +10,6 @@ function onSignInCallback(authResult) {
     //   "access_denied" - User denied access to your app
     //   "immediate_failed" - Could not automatically log in the user
     console.log('Sign-in state: ' + authResult['error']);
+    gapi.auth.signOut();    
   }
 }
