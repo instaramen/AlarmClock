d20b5cd526ffcc56c66ba7c9fdcdaeb57f8a7a3a
Peter Tran <tranpeter93@gmail.com>
2015-02-17 23:04:42 -0800

    testing deploy 4

--- js/gplus.js
+++ js/gplus.js
@@ -2,7 +2,7 @@ function onSignInCallback(authResult) {
   if (authResult['status']['signed_in']) {
     // Update the app to reflect a signed in user
     // Hide the sign-in button now that the user is authorized, for example:
-    document.getElementById('signinButton').setAttribute('style', 'display: none');
+    // document.getElementById('signinButton').setAttribute('style', 'display: none');
   } else {
     // Update the app to reflect a signed out user
     // Possible error values:
