b0e15d74e331567fb98616ac5c1a1a0345c55b76
Peter Tran <tranpeter93@gmail.com>
2015-02-17 23:01:03 -0800

    testing deploy 3

--- js/gplus.js
+++ js/gplus.js
@@ -1,109 +1,14 @@
-var helper = (function() {
-  var BASE_API_PATH = 'plus/v1/';
-
-  return {
-    /**
-     * Hides the sign in button and starts the post-authorization operations.
-     *
-     * @param {Object} authResult An Object which contains the access token and
-     *   other authentication information.
-     */
-    onSignInCallback: function(authResult) {
-      gapi.client.load('plus','v1').then(function() {
-        $('#authResult').html('Auth Result:<br/>');
-        for (var field in authResult) {
-          $('#authResult').append(' ' + field + ': ' +
-              authResult[field] + '<br/>');
-        }
-        if (authResult['access_token']) {
-          $('#authOps').show('slow');
-          $('#gConnect').hide();
-          helper.profile();
-          helper.people();
-        } else if (authResult['error']) {
-          // There was an error, which means the user is not signed in.
-          // As an example, you can handle by writing to the console:
-          console.log('There was an error: ' + authResult['error']);
-          $('#authResult').append('Logged out');
-          $('#authOps').hide('slow');
-          $('#gConnect').show();
-        }
-        console.log('authResult', authResult);
-      });
-    },
-
-    /**
-     * Calls the OAuth2 endpoint to disconnect the app for the user.
-     */
-    disconnect: function() {
-      // Revoke the access token.
-      $.ajax({
-        type: 'GET',
-        url: 'https://accounts.google.com/o/oauth2/revoke?token=' +
-            gapi.auth.getToken().access_token,
-        async: false,
-        contentType: 'application/json',
-        dataType: 'jsonp',
-        success: function(result) {
-          console.log('revoke response: ' + result);
-          $('#authOps').hide();
-          $('#profile').empty();
-          $('#visiblePeople').empty();
-          $('#authResult').empty();
-          $('#gConnect').show();
-        },
-        error: function(e) {
-          console.log(e);
-        }
-      });
-    },
-
-    /**
-     * Gets and renders the list of people visible to this app.
-     */
-    people: function() {
-      gapi.client.plus.people.list({
-        'userId': 'me',
-        'collection': 'visible'
-      }).then(function(res) {
-        var people = res.result;
-        $('#visiblePeople').empty();
-        $('#visiblePeople').append('Number of people visible to this app: ' +
-            people.totalItems + '<br/>');
-        for (var personIndex in people.items) {
-          person = people.items[personIndex];
-          $('#visiblePeople').append('<img src="' + person.image.url + '">');
-        }
-      });
-    },
-
-    /**
-     * Gets and renders the currently signed in user's profile data.
-     */
-    profile: function(){
-      gapi.client.plus.people.get({
-        'userId': 'me'
-      }).then(function(res) {
-        var profile = res.result;
-        $('#profile').empty();
-        $('#profile').append(
-            $('<p><img src=\"' + profile.image.url + '\"></p>'));
-        $('#profile').append(
-            $('<p>Hello ' + profile.displayName + '!<br />Tagline: ' +
-            profile.tagline + '<br />About: ' + profile.aboutMe + '</p>'));
-        if (profile.cover && profile.coverPhoto) {
-          $('#profile').append(
-              $('<p><img src=\"' + profile.cover.coverPhoto.url + '\"></p>'));
-        }
-      }, function(err) {
-        var error = err.result;
-        $('#profile').empty();
-        $('#profile').append(error.message);
-      });
-    }
-  };
-})();
-
 function onSignInCallback(authResult) {
-  helper.onSignInCallback(authResult);
+  if (authResult['status']['signed_in']) {
+    // Update the app to reflect a signed in user
+    // Hide the sign-in button now that the user is authorized, for example:
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
+  } else {
+    // Update the app to reflect a signed out user
+    // Possible error values:
+    //   "user_signed_out" - User is signed-out
+    //   "access_denied" - User denied access to your app
+    //   "immediate_failed" - Could not automatically log in the user
+    console.log('Sign-in state: ' + authResult['error']);
+  }
 }
