f3870258a9cef2beea9e12fb54f459cdd56a70ac
Peter Tran <tranpeter93@gmail.com>
2015-02-18 16:15:24 -0800

    testing 01

--- js/gplus.js
+++ js/gplus.js
@@ -3,16 +3,24 @@ function signinCallback(authResult) {
     // Update the app to reflect a signed in user
     console.log('Sign-in state: ' + authResult['error']);    
     console.log('hiding button...')
-    // $('#signinButton').attr('style', 'display: none');
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
   } else {
     console.log('Sign-in state: ' + authResult['error']);
+    document.getElementById('signinButton').setAttribute('style', 'display: block');    
   }
 }
 
 function render() {
+  var clientid = '309685935001-h38m2dmetejda1pj7992jifqla67a78e';
+
   var additionalParams = {
+    'clientid' : clientid + '.apps.googleusercontent.com',
+    'scope' : 'https://www.googleapis.com/auth/plus.login',
+    'requestvisibleactions' : 'http://schema.org/AddAction',
+    'cookiepolicy' : 'single_host_origin',
+    'callback' : 'signinCallback',
     'theme': 'dark'
   };
 
-  gapi.signin.render('signinButton');
+  gapi.signin.render('signinButton', additionalParams);
 }
