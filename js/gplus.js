87adefc6e38ad569aad2410bacb6b412df88bd26
Peter Tran <tranpeter93@gmail.com>
2015-02-19 23:22:17 -0800

    testing

--- js/gplus.js
+++ js/gplus.js
@@ -1,5 +1,3 @@
-var profile, email;
-
 function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
     console.log('hiding button...2');
@@ -7,7 +5,7 @@ function signinCallback(authResult) {
     // document.getElementById('signinButton').setAttribute('style', 'display: none');
     console.log('Sign-in state: ' + authResult['error']); 
 	if (authResult['error'] == undefined){
-        gapi.client.load('plus','v1', loadProfile);  // Trigger request to get the email address.
+        gapi.client.load('plus','v1');  // Trigger request to get the email address.
     } else {
         console.log('An error occurred');
     }
@@ -16,17 +14,4 @@ function signinCallback(authResult) {
   } else {
     console.log('Sign-in state: ' + authResult['error']);
   }
-}
-
-function loadProfile(){
-    var request = gapi.client.plus.people.get( {'userId' : 'me'} );
-    request.execute(loadProfileCallback);
-}
-
-function loadProfileCallback(obj) {
-    profile = obj;
-    email = obj['emails'].filter(function(v) {
-        return v.type === 'account'; // Filter out the primary email
-    })[0].value; // get the email from the filtered results, should always be defined.
-    $('#name').html(email);
 }
