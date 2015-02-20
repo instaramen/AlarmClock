a0f0e324fc740eb1b252f66e64752512704b1e7e
Peter Tran <tranpeter93@gmail.com>
2015-02-19 22:49:43 -0800

    testing

--- js/gplus.js
+++ js/gplus.js
@@ -1,9 +1,9 @@
 function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
     console.log('Sign-in state: ' + authResult['error']); 
-    console.log('Sign-in state: ' + authResult['status']);           
-    console.log('hiding button...2');
-    $('#signinButton').css('style', 'display: none');
+    console.log('hiding button...3');
+    // $('#signinButton').css('style', 'display: none');
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
   } else {
     console.log('Sign-in state: ' + authResult['error']);
   }
