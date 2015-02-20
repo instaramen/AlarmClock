5bee8830b1e170d2f6e989579ad5032bc5b74ead
Peter Tran <tranpeter93@gmail.com>
2015-02-19 23:41:36 -0800

    update

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,6 @@
 function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
-    console.log('hiding button...1');
+    console.log('hiding button...2');
     console.log('Sign-in state: ' + authResult['error']); 
 
     var requesturl = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=".concat(authResult.access_token);
