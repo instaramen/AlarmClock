ef858a09c2d37855596cbc0c71268af46d4d031f
Peter Tran <tranpeter93@gmail.com>
2015-02-19 22:09:42 -0800

    testing

--- js/gplus.js
+++ js/gplus.js
@@ -2,7 +2,7 @@ function signinCallback(authResult) {
   if (authResult['status']['signed_in']) {
     // Update the app to reflect a signed in user
     console.log('Sign-in state: ' + authResult['error']);    
-    console.log('hiding button...7')
+    console.log('hiding button...0')
     $('#signinButton').css('style', 'display: none');
   } else {
     console.log('Sign-in state: ' + authResult['error']);
